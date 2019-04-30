bb734af0050f489f4d83052348410b3c521cf2c6
valentinachavez <valencha10@gmail.com>
2019-04-29 21:16:08 -0500

    Vista de producto lista

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 var express = require('express');
 var motorRender = require('express-handlebars');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const dbName = 'tienda';
+const client = new MongoClient(url);
+client.connect(function(err){
+  
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  const articulos = db.collection('productos');
+  articulos.find({}, {sort: ['precio']}).toArray(function(err,docs){
+    assert.equal(null,err);
+    console.log('encontre los docs');
+    docs.forEach(function(prod){
+      console.log(prod.precio);
+    });
+  });
+
+
+  client.close();
+
+});
+
 var app = express();
 
 var fs = require('fs');
@@ -26,6 +52,7 @@ app.get('/', function (req, res) {
 app.get('/store', function (req, res) {
     var contexto = {
       categoria: "All",
+      titulo:"All",
       listaProductos: productos,
     };
     res.render('lista-productos', contexto);
@@ -39,18 +66,35 @@ app.get('/store/:categoria', function (request, res) {
   productos.forEach(function (producto) {
     if (producto.tipo == request.params.categoria) {
       arrayProductos.push(producto);
+      
     }
     
 
   });
 
 
+  var titulo = '';
+  if(request.params.categoria == 'food'){
+    titulo = 'Food';
+  }
+
+  if(request.params.categoria == 'clothing'){
+    titulo = 'Clothing';
+  }
+
+  if(request.params.categoria == 'toys'){
+    titulo = 'Toys';
+  }
+  if(request.params.categoria == 'forwalks'){
+    titulo = 'For Walks';
+  }
 
   var contexto = {
+    
     categoria: request.params.categoria,
+    titulo: titulo,
     listaProductos: arrayProductos,
   };
-
 
   res.render('lista-productos', contexto);
 
