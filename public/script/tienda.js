4a3239ccfa98565c9c69681ffed2fef8490863bc
valentinachavez <valencha10@gmail.com>
2019-05-31 01:36:37 -0500

    Ojala funcione

--- public/script/tienda.js
+++ public/script/tienda.js
@@ -129,7 +129,8 @@ function paginaCargada(){
                     if(inputDescuento.value=='CANS1'){
                  
                         estado.style.backgroundImage = "url('/images/check.gif')";
-                        TweenMax.to(estado,1, {opacity:1, repeat:1},0.5);
+                        tl2.to(estado,1, {opacity:0, ease:SteppedEase.config(16)},0.6);
+                        tl2.repeat(1);
                       var des= sumaTotal*0.2;
                       var entero = parseInt(des);
                       dis.innerHTML = "20%";
