d17cfca0e8d3fcac612ed8f5eb1988580149d9eb
isabellajordan <isabellajordanbe@gmail.com>
2019-05-31 13:51:13 -0500

    ño

--- public/script/animacionLetra.js
+++ public/script/animacionLetra.js
@@ -5,19 +5,20 @@ var slot= document.querySelector('.slot');
 
 TweenMax.from(titulo, 1, { opacity: 0 , x: -2000});
 
+/*
 function mover(){
 
-    
 
     }
+    */
 
     /*
     function caidaBalon(){
     TweenMax.to(titulo, 3, {y:450});
     }
     */
-
-  window.addEventListener('click',mover);
-  
+/*
+  window.addEventListener('mouseenter',mover);
+  */
  
 
